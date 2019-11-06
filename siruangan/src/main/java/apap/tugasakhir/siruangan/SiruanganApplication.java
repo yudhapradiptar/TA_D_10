dd0ba1214c6d1373d2372048564c8bef7d10d444
Yudha Pradipta Ramadan <yudha512000@gmail.com>
2019-11-06 15:44:04 +0700

    create-project

---
+++ siruangan/src/main/java/apap/tugasakhir/siruangan/SiruanganApplication.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.siruangan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiruanganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiruanganApplication.class, args);
+	}
+
+}
---
+++ siruangan/src/test/java/apap/tugasakhir/siruangan/SiruanganApplicationTests.java
@@ -0,0 +1,16 @@
+package apap.tugasakhir.siruangan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiruanganApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
