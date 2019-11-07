e200b7ce244c72857e6cb40041d35aa1f53737a3
irenepixel <irenepixelyne@gmail.com>
2019-11-07 23:14:36 +0700

    feat: modify PageController.java file

--- siruangan/src/main/java/apap/tugasakhir/siruangan/controller/PageController.java
+++ siruangan/src/main/java/apap/tugasakhir/siruangan/controller/PageController.java
@@ -1,6 +1,5 @@
 package apap.tugasakhir.siruangan.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController{
-    @Autowired
-
     @RequestMapping("/")
-    public String home (Model model){
+    public String home (){
         return "home";
     }
 }
