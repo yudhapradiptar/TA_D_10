3f5d01ed1d1c75c28ea35d087231e896456333b1
irenepixel <irenepixelyne@gmail.com>
2019-11-20 15:23:27 +0700

    feat: add GuruDetail and SiswaDetail file

---
+++ siruangan/src/main/java/apap/tugasakhir/siruangan/rest/SiswaDetail.java
@@ -0,0 +1,81 @@
+package apap.tugasakhir.siruangan.rest;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.Date;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class SiswaDetail {
+    private String uuidUser;
+
+    public String getUuidUser() {
+        return uuidUser;
+    }
+
+    public void setUuidUser(String uuidUser) {
+        this.uuidUser = uuidUser;
+    }
+
+    private String nis;
+
+    public String getNis() {
+        return nis;
+    }
+
+    public void setNis(String nis) {
+        this.nis = nis;
+    }
+
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    private String tempatLahir;
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date tanggalLahir;
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    private String alamat;
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    private String telepon;
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+}
