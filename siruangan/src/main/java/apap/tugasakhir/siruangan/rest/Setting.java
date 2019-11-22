32a282ccaf53476e82bb07fcaf2fe738a3a91425
yudhapradiptar <50763128+yudhapradiptar@users.noreply.github.com>
2019-11-22 22:00:44 +0700

    Update Setting.java

--- siruangan/src/main/java/apap/tugasakhir/siruangan/rest/Setting.java
+++ siruangan/src/main/java/apap/tugasakhir/siruangan/rest/Setting.java
@@ -2,6 +2,6 @@ package apap.tugasakhir.siruangan.rest;
 
 public class Setting {
     final public static String bukuUrl = "https://ebef3c71-2d3d-48a6-89be-303c60c1beb0.mock.pstmn.io/";
-}
+
     final public static String sivitasUrl = "http://sivitas.herokuapp.com/";
 }
