fb69363e7d6c39ae729dbdbbb89b5e7aab17e595
Aghni Anugrah Raesa <anugrah.raesa@gmail.com>
2019-12-06 17:52:01 +0700

    Update FasilitasRuanganRestController.java

--- src/main/java/apap/tugasakhir/siruangan/controller/FasilitasRuanganRestController.java
+++ src/main/java/apap/tugasakhir/siruangan/controller/FasilitasRuanganRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/api/v1")
 public class FasilitasRuanganRestController {
     @Autowired
